0363cc9c97250db1dd732cb768027619e9dc0866
tmnkakr <tmnkakr@gmail.com>
2022-04-26 10:11:47 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Test from './test1';
 import Test2 from './test2';
 
@@ -12,28 +9,19 @@ import Test2 from './test2';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Test />
-      </Route>
-      <Route path="/about">
-        <Test2 />
-      </Route>
-    </Switch>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Test />
+        </Route>
+        <Route path="/about">
+          <Test2 />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 
 
 
-// const rootElement = document.getElementById("root");
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} />
-    </Routes>
-  </BrowserRouter>,
-  rootElement
-);
--- src/test1.js
+++ src/test1.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export default () => {
+const test1= () => {
     return(
         <div>test1</div>
     )
 
 }
+
+export default test1;
